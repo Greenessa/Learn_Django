197562f48a4cfa5456a82a48f0971f79f8034aa9
Greenessa <romanova184@yandex.ru>
2023-11-24 15:54:00 +0300

    second commit

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,5 +1,10 @@
+import csv
+
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
+
+from pagination import settings
 
 
 def index(request):
@@ -7,11 +12,35 @@ def index(request):
 
 
 def bus_stations(request):
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
-
-    context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
-    }
+    with open(settings.BUS_STATION_CSV, newline='', encoding='utf-8') as csvfile:
+        reader = list(csv.DictReader(csvfile))
+        # получите текущую страницу и передайте ее в контекст
+        # также передайте в контекст список станций на странице
+        paginator = Paginator(reader, 10)
+        page_number = int(request.GET.get('page', 1))
+        page=paginator.get_page(page_number)
+        context = {
+            'bus_stations': page.object_list,
+            'page': page,
+        }
     return render(request, 'stations/index.html', context)
+
+
+# contact_list = Women.objects.all()
+# paginator = Paginator(contact_list, 3)
+#
+# page_number = request.GET.get('page')
+# page_obj = paginator.get_page(page_number)
+# list = []
+# dict = {}
+# with open(settings.BUS_STATION_CSV, newline='', encoding='utf-8') as csvfile:
+#     reader = csv.DictReader(csvfile)
+#     #print(reader)
+#     for row in reader:
+#         print(row)
+#         dict['Name'] = row['Name']
+#         dict['Street'] = row['Street']
+#         dict['District'] = row['District']
+        #print(dict)
+        #list.append(dict)
+        #print(list)
