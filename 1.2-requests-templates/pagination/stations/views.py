acc16604b6e7c34f1b90d096ea5a8b71aca53d1a
Greenessa <romanova184@yandex.ru>
2023-11-24 16:09:32 +0300

    next commit

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -26,21 +26,3 @@ def bus_stations(request):
     return render(request, 'stations/index.html', context)
 
 
-# contact_list = Women.objects.all()
-# paginator = Paginator(contact_list, 3)
-#
-# page_number = request.GET.get('page')
-# page_obj = paginator.get_page(page_number)
-# list = []
-# dict = {}
-# with open(settings.BUS_STATION_CSV, newline='', encoding='utf-8') as csvfile:
-#     reader = csv.DictReader(csvfile)
-#     #print(reader)
-#     for row in reader:
-#         print(row)
-#         dict['Name'] = row['Name']
-#         dict['Street'] = row['Street']
-#         dict['District'] = row['District']
-        #print(dict)
-        #list.append(dict)
-        #print(list)
